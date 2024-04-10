c535dd77b36228eed4e72a8d27c767583659d57c
pmj_15643430352 <2933151435@qq.com>
2024-04-10 22:47:56 +0800

    feat:首页添加轮播图

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,34 @@
+
+
+import { Swiper, Toast } from 'antd-mobile'
+import './home.css'
+
 export function Home () {
+  const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+  const items = colors.map((color, index) => (
+    <Swiper.Item key={index}>
+      <div
+        className='content'
+        style={{ background: color }}
+        onClick={() => {
+          Toast.show(`你点击了卡片 ${index + 1}`)
+        }}
+      >
+        {index + 1}
+      </div>
+    </Swiper.Item>
+  ))
   return (
-    <div>home</div>
+    <div className='homeBox'>
+      <Swiper
+        loop
+        autoplay
+        onIndexChange={i => {
+          console.log(i, 'onIndexChange1')
+        }}
+      >
+        {items}
+      </Swiper>
+    </div>
   )
 }
